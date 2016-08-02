25ab255f4607a157a0d73b3762e187df43f5750e
Brian Judd <bjudd21@gmail.com>
2016-08-02 10:19:10 -0400

    initial commit

---
+++ javascripting/array-filtering.js
@@ -0,0 +1,12 @@
+var numbers = [1,2,3,4,5,6,7,8,9,10];
+
+function evenNumbers(numbers){
+	return number %2 ===0;
+}
+
+
+
+var filtered = numbers.filter(evenNumbers(number));
+
+
+console.log(filtered);
